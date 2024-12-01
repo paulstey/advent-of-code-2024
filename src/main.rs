c359966e9e5234aecb21776e00211c06d6e54fec
Paul Stey <paulstey@gmail.com>
2024-12-01 16:50:48 -0500

    feat: solve part 2 of day 1

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,10 @@ use std::io::{self, BufRead};
 use std::time::Instant;
 
 fn read_two_columns() -> io::Result<(Vec<i32>, Vec<i32>)> {
-    // Specify the path to the file
     let path = "data/input.txt";
-
-    // Open the file in read-only mode
     let file = File::open(&path)?;
-
-    // Create a buffered reader for efficient reading
     let reader = io::BufReader::new(file);
 
-    // Initialize a vector to store pairs of integers
     let mut column1: Vec<i32> = Vec::new();
     let mut column2: Vec<i32> = Vec::new();
 
@@ -21,7 +15,6 @@ fn read_two_columns() -> io::Result<(Vec<i32>, Vec<i32>)> {
 
         let tokens: Vec<&str> = line.trim().split_whitespace().collect();
 
-        // Parse the tokens into integers
         let a = match tokens[0].parse::<i32>() {
             Ok(num) => num,
             Err(_) => {
@@ -49,17 +42,17 @@ fn main() {
     let t1 = Instant::now();
     let (mut column1, mut column2) = read_two_columns().unwrap();
 
+    // The following two lines were only needed for Part 1 of today's problem.
     column1.sort();
     column2.sort();
 
-    let mut total_difference = 0;
-
-    for (a, b) in column1.iter().zip(column2.iter()) {
-        total_difference += (a - b).abs();
-    }
+    let similarity_score: i32 = column1
+        .iter()
+        .map(|&needle| needle * (column2.iter().filter(|&&elem| elem == needle).count() as i32))
+        .sum();
 
     let elapsed = t1.elapsed();
 
-    println!("Total difference: {}", total_difference);
+    println!("Total difference: {}", similarity_score);
     println!("Elapsed time: {:?}", elapsed);
 }
