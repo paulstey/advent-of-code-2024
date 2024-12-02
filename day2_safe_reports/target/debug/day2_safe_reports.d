34f2472ea5519a6bc0e0ce9c15ba33391fd0ee1e
Paul Stey <paulstey@gmail.com>
2024-12-02 11:27:04 -0500

    chore: remove target directory from day2

--- day2_safe_reports/target/debug/day2_safe_reports.d
+++
@@ -1,1 +0,0 @@
-/Users/pstey/projects/advent-of-code-2024/day2_safe_reports/target/debug/day2_safe_reports: /Users/pstey/projects/advent-of-code-2024/day2_safe_reports/src/main.rs
--- day2_safe_reports/target/debug/deps/day2_safe_reports-a99b65a29fabc305.d
+++
@@ -1,5 +0,0 @@
-/Users/pstey/projects/advent-of-code-2024/day2_safe_reports/target/debug/deps/day2_safe_reports-a99b65a29fabc305: src/main.rs
-
-/Users/pstey/projects/advent-of-code-2024/day2_safe_reports/target/debug/deps/day2_safe_reports-a99b65a29fabc305.d: src/main.rs
-
-src/main.rs:
